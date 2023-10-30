87e66cdc7da3ee69855efcd939bca462db8a974e
Sky Yin <sky.yin@gmail.com>
2023-10-29 19:52:57 -0700

    Standalone function

--- hands-on-rust/treehouse/src/main.rs
+++ hands-on-rust/treehouse/src/main.rs
@@ -1,12 +1,16 @@
 use std::io::stdin;
 
-fn main() {
-    println!("Hello, what's your name?");
-
+fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
+    your_name
+}
+fn main() {
+    println!("Hello, what's your name?");
 
-    println!("Hello, {}", your_name)
+    let name = what_is_your_name();
+
+    println!("Hello, {}", name)
 }

eb43f79f23bcee2de1a55a139e3d21cc9ca0ef3f
Sky Yin <sky.yin@gmail.com>
2024-01-16 22:33:31 -0800

    borrowed value error

--- rust-bible/ownership/src/main.rs
+++ rust-bible/ownership/src/main.rs
@@ -4,12 +4,24 @@
 // - Whenever the owner leaves the scope, the value will be dropped
 // - Stack and heap exhibit different copy behaviors
 
+// fn main() {
+//     let s = "hello"; // static compiled string, immutable
+
+//     let mut t = String::from("hello"); // String is dynamically allocated on heap, thus expandable
+//     t.push_str(", world!");
+
+//     let t2 = t; // this is called "move"
+//     println!("{}", t); // error: borrow of moved value: `t`
+// }
+
 fn main() {
-    let s = "hello"; // static compiled string, immutable
+    let s = String::from("hello");
+    take_ownership(s); // move
 
-    let mut t = String::from("hello"); // String is dynamically allocated on heap, thus expandable
-    t.push_str(", world!");
+    // moved value won't automatically be moved back
+    println!("{}", s); // error: value borrowed after move
+}
 
-    let t2 = t; // this is called ownership "move"
-    println!("{}", t); // error: borrow of moved value: `t`
+fn take_ownership(s: String) {
+    println!("{}", s);
 }
